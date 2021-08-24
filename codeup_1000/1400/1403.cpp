290fad9ba065d768817fbdb4da2cd84a25dd8b13
kimsumin <ksm4465ksm@gmail.com>
2021-08-25 03:40:35 +0900

    1D Array

---
+++ codeup_1000/1400/1403.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,cnt=0;
+	cin >> n;
+	if (n > 1000 || n<1)
+		cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+
+		cin>>arr[i];
+	}
+
+	for (int )
+	delete[] arr;
+
+}
